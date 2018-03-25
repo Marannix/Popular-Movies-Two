aff423d3688bd69937c956ed73508359bcfbcff4
Josh <joshua@memrise.com>
2018-03-25 21:49:35 +0100

    Added Api for Review and Videos
    Added the retrofit get to retrieve the videos and reviews of specific movies.

--- app/src/main/java/com/example/tobi/popular_movies_1/MovieApi.java
+++ app/src/main/java/com/example/tobi/popular_movies_1/MovieApi.java
@@ -2,6 +2,7 @@ package com.example.tobi.popular_movies_1;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
 
 /**
  * Created by Tobi on 25-Feb-18.
@@ -9,12 +10,21 @@ import retrofit2.http.GET;
 
 public interface MovieApi {
 
-  //Insert your Api Key or crash
-  String apiKey = "";
+    //Insert your Api Key or crash
+    String apiKey = "";
 
-  String authentication = "api_key=" + apiKey;
+    String authentication = "api_key=" + apiKey;
 
-  @GET("movie/popular?" + authentication) Call<MovieResponse> getPopularMovies();
-  @GET("movie/top_rated?" + authentication) Call<MovieResponse> getTopRatedMovies();
+    @GET("movie/popular?" + authentication)
+    Call<MovieResponse> getPopularMovies();
+
+    @GET("movie/top_rated?" + authentication)
+    Call<MovieResponse> getTopRatedMovies();
+
+    @GET("movie/{id}/reviews?" + authentication)
+    Call<MovieResponse> getMovieReviews(@Path("id") int id);
+
+    @GET("movie/{id}/videos?" + authentication)
+    Call<MovieResponse> getMovieVideos(@Path("id") int id);
 
 }
